4e9ecde7499a6af77cbcce247e96e76c175d864f
Saidafzal Kholkhujaev <saidafzal1209@gmail.com>
2023-07-10 19:47:07 +0500

    vercel

--- types/environment.d.ts
+++ types/environment.d.ts
@@ -3,7 +3,13 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      DATABASE_URL: string;
+      POSTGRES_URL: string;
+      POSTGRES_PRISMA_URL: string;
+      POSTGRES_URL_NON_POOLING: string;
+      POSTGRES_USER: string;
+      POSTGRES_HOST: string;
+      POSTGRES_PASSWORD: string;
+      POSTGRES_DATABASE: string;
 
       GOOGLE_CLIENT_ID: string;
       GOOGLE_CLIENT_SECRET: string;
